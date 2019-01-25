309e4d9e26671b3957214b5476ab023ef0f7138a
Louie O'Grady <l.ogrady@gmail.com>
2019-01-25 13:48:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-while number >= 0
-puts "#{number} SECOND(S)!"
-number -= 1
-end
-return "HAPPY NEW YEAR!"
-end
+  while number >= 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  end
